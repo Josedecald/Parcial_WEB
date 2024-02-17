dcdbb99289505b1b3e0c6890daf37f1da5792f59
SMarlboro <sebascastro2222@gmail.com>
2024-02-17 00:14:09 -0500

    login no terminaod

--- js/login.js
+++ js/login.js
@@ -2,9 +2,33 @@ console.log('Vinculado');
 
 let users = [];
 
+//  se utiliza fetch para obtener los datos de usuarios
+fetch('../json/users.json')
+    .then(response => {
+        if (!response.ok){
+            throw new Error('Error al obtener datos de usuarios.');
+        }
+        return response.json();
+    })
+    .then(json => {
+        console.log('Datos de usuarios obtenidos:', json);
+        // Verificamos si los datos son un arreglo
+        if (Array.isArray(json)) {
+            // Si son un array, los asignamos directamente a users
+            users = json;
+            console.log('Usuarios cargados:', users);
+        } else {
+            // Si no son un array, los metemos en uno
+            users = [json];
+            console.log('Usuarios cargados:', users);
+        }
+    })
+    .catch(err => console.error('Solicitud fallida: ', err));
+
+
 let validateLogin = () => {
-    let user = document.getElementById("txt_username").value.trim();
-    let pass = document.getElementById("txt_password").value.trim();
+    let user = document.getElementById("txt_user").value;
+    let pass = document.getElementById("txt_pass").value;
 
     if (user === '' || pass === '') {
         alert('Por favor, ingrese nombre de usuario y contraseña.');
@@ -22,27 +46,7 @@ let validateLogin = () => {
     }
 };
 
-//  se utiliza fetch para obtener los datos de usuarios
-fetch('../json/users.json')
-    .then(response => {
-        if (!response.ok){
-            throw new Error('Error al obtener datos de usuarios.');
-        }
-        return response.json();
-    })
-    .then(json => {
-        console.log('Datos de usuarios obtenidos:', json);
-        // Verificamos si los datos son un arreglo
-        if (Array.isArray(json)) {
-            // Si son un array, los asignamos directamente a users
-            users = json;
-        } else {
-            // Si no son un array, los metemos en uno
-            users = [json];
-        }
-        console.log('Usuarios cargados:', users);
-    })
-    .catch(err => console.error('Solicitud fallida: ', err));
+
 
 let redirectUser = () =>{
     window.location.href = '../index.html';
